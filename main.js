6cbb2a96857326a39c8abcddce6af9e7171bf0c5
Matt Terski <matt.terski@gmail.com>
2020-11-07 17:33:31 -0600

    feat: Mocks and resolvers

--- main.js
+++ main.js
@@ -3,10 +3,26 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
     type Query {
         hello: String
+        helloTwo: String
     }
 `;
 
-const server = new ApolloServer({ typeDefs, mocks: true });
+const mocks = {
+    String: () => 'Hello mocks',
+};
+
+const resolvers = {
+    Query: {
+        helloTwo: () => 'Hello Two',
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    mocks,
+    resolvers,
+    mockEntireSchema: false,
+});
 
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
